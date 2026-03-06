5ddcf91ab595eaab010e931e1c7e6c73aa4ca373
thejagowd <thejagowdvn@gmail.com>
2026-03-06 13:13:11 +0530

    Update variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sql_server_name" {
+  type = string
+}
+
+variable "database_name" {
+  type = string
+}
+
+variable "admin_login" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
