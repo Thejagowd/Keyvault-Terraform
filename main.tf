5c8a995ee4b9e64e864833ab5c8058138ce684b8
thejagowd <thejagowdvn@gmail.com>
2026-03-06 13:11:49 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.admin_login
+  administrator_login_password = var.admin_password
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name           = var.database_name
+  server_id      = azurerm_mssql_server.sqlserver.id
+  sku_name       = "S0"
+  max_size_gb    = 2
+}
